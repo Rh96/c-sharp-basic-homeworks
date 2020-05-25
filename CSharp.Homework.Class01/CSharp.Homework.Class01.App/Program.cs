14a108109a4d2acf2547af4eb86a8a65c6df51d2
Rade <radehrgo96@gmail.com>
2020-05-25 14:59:43 +0200

    Added Homework Class 01

---
+++ CSharp.Homework.Class01/CSharp.Homework.Class01.App/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace CSharp.Homework.Class01.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Traffic light...\n");
+
+            Thread.Sleep(3000);
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("Red light - STOP!\n");
+
+            Thread.Sleep(3000);
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Yellow light - PREPARE!\n");
+
+            Thread.Sleep(3000);
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("Green light - GO!");
+
+            Console.ReadLine();
+        }
+    }
+}
