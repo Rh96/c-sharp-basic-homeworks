ae58a9e51b2559816e4f5a15f852ce23bd0a9774
Rade <radehrgo96@gmail.com>
2020-05-25 20:07:01 +0200

    Added Homework Class 03

---
+++ CSharp.Homework.Class03/CSharp.Homework.Class03.Task01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CSharp.Homework.Class03.Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOfIntegers = new int[6];
+            int sumOfEven = 0;
+
+            Console.WriteLine("Enter 6 numbers: ");
+
+            int i = 0;
+            while(i < arrayOfIntegers.Length)
+            {
+                bool isParsed = int.TryParse(Console.ReadLine(), out arrayOfIntegers[i]);
+                i++;
+            }
+
+            i = 0;
+            while(i < 6)
+            {
+                if(arrayOfIntegers[i] % 2 == 0)
+                {
+                    sumOfEven += arrayOfIntegers[i];
+                }
+                i++;
+            }
+            Console.WriteLine("The sum of even numbers in array is: " + sumOfEven);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp.Homework.Class03/CSharp.Homework.Class03.Task02/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CSharp.Homework.Class03.Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] group1 = new string[]
+            {
+                "Rade","Mare","Trale","Stojancho","Ivko",
+            };
+
+            string[] group2 = new string[]
+            {
+                "Zdravko","Petko","Stanko","Branko","Trajko",
+            };
+
+            Console.WriteLine("Enter student group: (there are 1 and 2)");
+            bool isParsed = int.TryParse(Console.ReadLine(), out int userChoice);
+
+            if (userChoice == 1)
+            {
+                Console.WriteLine("Students from group 1:");
+                foreach (var item in group1)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            if (userChoice == 2)
+            {
+                Console.WriteLine("Students from group 2:\n");
+                foreach (var item in group2)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
