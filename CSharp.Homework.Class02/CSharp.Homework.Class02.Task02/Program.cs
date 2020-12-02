ca95b7f47f41e310538a952626cbcbd78a7ae752
Rade <radehrgo96@gmail.com>
2020-12-02 16:09:23 +0100

    Class02 Homework AverageNumber Complete

--- CSharp.Homework.Class02/CSharp.Homework.Class02.Task02/Program.cs
+++ CSharp.Homework.Class02/CSharp.Homework.Class02.Task02/Program.cs
@@ -6,19 +6,21 @@ namespace CSharp.Homework.Class02.Task02
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter the first number: ");
-            bool isParsed = int.TryParse(Console.ReadLine(), out int firstNumber);
-            Console.Write("Enter the second number: ");
-            bool isParsed2 = int.TryParse(Console.ReadLine(), out int secondNumber);
-            Console.Write("Enter the third number: ");
-            bool isParsed3 = int.TryParse(Console.ReadLine(), out int thirdNumber);
-            Console.Write("Enter the fourth number: ");
-            bool isParsed4 = int.TryParse(Console.ReadLine(), out int fourthNumber);
+            Console.WriteLine("Enter first number:");
+            int.TryParse(Console.ReadLine(), out int num1);
 
-            int average = (firstNumber + secondNumber + thirdNumber + fourthNumber) / 4;
+            Console.WriteLine("Enter second number:");
+            int.TryParse(Console.ReadLine(), out int num2);
 
-            Console.WriteLine("The average of " + 
-                firstNumber + ", " + secondNumber + ", " + thirdNumber + " and " + fourthNumber + " is: " + average);
+            Console.WriteLine("Enter third number:");
+            int.TryParse(Console.ReadLine(), out int num3);
+
+            Console.WriteLine("Enter fourth number:");
+            int.TryParse(Console.ReadLine(), out int num4);
+
+            int average = (num1 + num2 + num3 + num4) / 4;
+
+            Console.WriteLine("The average of {0}, {1}, {2} and {3} is: {4}", num1, num2, num3, num4, average);
 
             Console.ReadLine();
         }
