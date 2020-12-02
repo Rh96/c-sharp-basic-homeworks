c40875c0985010b72c1e362451bfea4eedfa0d1d
Rade <radehrgo96@gmail.com>
2020-12-02 16:11:33 +0100

    Class02 Homework SwapNumbers Complete

--- CSharp.Homework.Class02/CSharp.Homework.Class02.Task03/Program.cs
+++ CSharp.Homework.Class02/CSharp.Homework.Class02.Task03/Program.cs
@@ -6,19 +6,18 @@ namespace CSharp.Homework.Class02.Task03
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter the first number: ");
-            bool isParsed = int.TryParse(Console.ReadLine(), out int firstNumber);
-            Console.Write("Enter the second number: ");
-            bool isParsed2 = int.TryParse(Console.ReadLine(), out int secondNumber);
+            Console.WriteLine("Enter first number:");
+            int.TryParse(Console.ReadLine(), out int num1);
 
+            Console.WriteLine("Enter second number:");
+            int.TryParse(Console.ReadLine(), out int num2);
 
-            Console.WriteLine("After swapping:");
-            int temp = 0;
-            temp = firstNumber; // 5
-            firstNumber = secondNumber; // 8
-            secondNumber = temp; // 5
-            Console.WriteLine("First number: " + firstNumber);
-            Console.WriteLine("Second number: " + secondNumber);
+            int temp;
+            temp = num1; // temp = 5
+            num1 = num2; // num1 = 8
+            num2 = temp; // num2 = 5
+
+            Console.WriteLine("After swapping:\nFirst number: {0}\nSecond number: {1}", num1, num2);
 
 
             Console.ReadLine();
